a1be7947779e56c92067d0fde3d6852ba70d935c
AyindeSegun <ayindesegun17@gmail.com>
2022-03-03 16:47:24 +0100

    first commit

---
+++ calculator.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended : true}))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/', (req, res) => {
+  const num1 = Number(req.body.num1)
+  const num2 = Number(req.body.num2)
+  const result = num1 + num2
+  res.send("The result of this calculation is " + result)
+})
+
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmiCalculator.html')
+})
+app.post('/bmicalculator', (req, res) => {
+  const weight = Number(req.body.weight)
+  const height = Number(req.body.height)
+  const result = weight / (height * heightit)
+  res.send('The result of your BMI is ' + result)
+})
+
+
+app.listen(3000, () => {
+  console.log("Hannah has a big head!!!!!!!!")
+})
